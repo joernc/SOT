0ffaaa79dd96dd88c8473a5605e719c2c3e10486
Jörn Callies <jcallies@caltech.edu>
2025-04-25 12:17:37 -0700

    Exporting findpairs.

--- src/SOT.jl
+++ src/SOT.jl
@@ -4,7 +4,7 @@ using SeisIO, LightXML, CSV, DataFrames, HDF5, Dates
 using PyPlot, PyCall, Printf, ProgressMeter
 using LinearAlgebra, SparseArrays, FFTW, Statistics, DSP
 
-export downloadseisdata, cutpwaves, measurepairs
+export downloadseisdata, cutpwaves, findpairs, measurepairs
 export twavepick
 export collectpairs, invert, correctcycleskipping, lineartrend, regulargrid, estimatetrend
 
